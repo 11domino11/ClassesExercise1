eda43ccea8d8eb7c4a1f8f9ca4c33e63c192041c
Damion Russell <domino.russell@gmail.com>
2022-07-20 11:51:51 -0400

    initial commit

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car buickRegal98 = new Car("Buick","Regal", 1998);
+            Console.WriteLine($"{ buickRegal98.Make} {buickRegal98.Model} made in {buickRegal98.Year}");
+
+            Car hondaAccord09 = new Car();
+                hondaAccord09.Make = "Honda";
+                hondaAccord09.Model = "Accord";
+                hondaAccord09.Year = 2009;
+            
+            Console.WriteLine(hondaAccord09.Make + hondaAccord09.Model + hondaAccord09.Year);
+            Console.ReadLine();
+        }
+    }
+}
